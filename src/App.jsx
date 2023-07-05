b364a9119656e9cdbd6783545764e21bf3a46b43
鄧莉瀕 <k18382003@gmail.com>
2023-07-05 12:24:09 -0700

    Adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 import data from './data.js';
 import List from './List.jsx';
+import Footer from './Footer.jsx';
 
 const App = () => {
   const [people, setPeople] = useState(data)
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} Birthday Today</h3>
-        <List people={people}/>
-      <button type='button' 
-      className='btn btn-block'
-      onClick={() => {setPeople([])}}
-      >Clear all</button>
-      </section>
-    </main>
-  )
+    <>
+      <main>
+        <section className="container">
+          <h3>{people.length} Birthday Today</h3>
+          <List people={people} />
+          <button
+            type="button"
+            className="btn btn-block"
+            onClick={() => {
+              setPeople([]);
+            }}
+          >
+            Clear all
+          </button>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,45 @@
+
+export default function Footer() {
+    const PageLinks = [{ id: 1, href: '#home', text: 'home' }];
+
+    const SocialIcons = [
+    {
+        id: 1,
+        href: 'https://github.com/k18382003/BirthdayList',
+        name: 'fab fa-github',
+    },
+    ];
+  return (
+    <>
+      <footer className="section footer">
+        <ul className="footer-icons">
+          {SocialIcons.map((icon) => {
+            return (
+              <li key={icon.id}>
+                <a
+                  href={icon.href}
+                  target="_blank"
+                  className="footer-icon"
+                  rel="noreferrer"
+                >
+                  <i className={icon.name}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="footer-links">
+          {PageLinks.map((link) => {
+            return (
+              <li key={link.href}>
+                <a href={link.href} className="footer-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </footer>
+    </>
+  );
+}
